89d5ca737f1a7978d4c68506c7298cb7f6d45a8a
MukilSaravanan <senjerimalaimukil@gmail.com>
2022-05-17 15:35:43 +0530

    Initial Commit

---
+++ build/hallucination/CMakeFiles/LfH_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/mukil/ros/jackal_ws/devel/share/common-lisp/ros/LfH/msg/Bspline.lisp"
+  "CMakeFiles/LfH_generate_messages_lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/LfH_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/LfH/cmake/LfH-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(LfH_MESSAGE_FILES "/home/mukil/ros/jackal_ws/src/hallucination/msg/Bspline.msg")
+set(LfH_SERVICE_FILES "")
